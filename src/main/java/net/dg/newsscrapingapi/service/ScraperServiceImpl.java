20033fa7a4c6fafec2fb987bc292df00db5188ce
Dragnea Alexandru <alexdg722@gmail.com>
2023-03-12 00:16:06 +0200

    added service impl

--- src/main/java/net/dg/newsscrapingapi/service/ScraperServiceImpl.java
+++ src/main/java/net/dg/newsscrapingapi/service/ScraperServiceImpl.java
@@ -1,9 +1,12 @@
 package net.dg.newsscrapingapi.service;
 
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 import lombok.AllArgsConstructor;
 import net.dg.newsscrapingapi.model.News;
+import net.dg.newsscrapingapi.utility.UtilityClass;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
@@ -17,6 +20,19 @@ public class ScraperServiceImpl implements ScraperService {
 
   @Override
   public Set<News> scrapeNews() {
-    return null;
+    Set<News> newsSet = new HashSet<>();
+
+    for (String url : urls) {
+
+      if (url.contains("mashable")) {
+
+        UtilityClass.extractDataFromMashable(newsSet, url);
+      } else if (url.contains("gizmodo")) {
+        UtilityClass.extractDataFromGizmodo(newsSet, url);
+      }
+
+    }
+
+    return newsSet;
   }
 }
