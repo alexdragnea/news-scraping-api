4bfb5d4fac7d1a617bf58428b3055263cd38e124
Alexandru Marian Dragnea <Alexandru.Dragnea@endava.com>
2023-03-13 16:21:32 +0200

    fix persistance

--- src/main/java/net/dg/newsscrapingapi/repository/NewsRepository.java
+++ src/main/java/net/dg/newsscrapingapi/repository/NewsRepository.java
@@ -4,5 +4,10 @@ import net.dg.newsscrapingapi.model.News;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
-public interface NewsRepository extends JpaRepository<News, Long> {}
+public interface NewsRepository extends JpaRepository<News, Long> {
+
+    boolean findNewsByTitle(String title);
+}
