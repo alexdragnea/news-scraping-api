4d2afa09496f9c87d10707c898e782454da2f69e
Alexandru Marian Dragnea <Alexandru.Dragnea@endava.com>
2023-03-13 15:17:26 +0200

    added entity annotations for News model

--- src/main/java/net/dg/newsscrapingapi/model/News.java
+++ src/main/java/net/dg/newsscrapingapi/model/News.java
@@ -2,23 +2,35 @@ package net.dg.newsscrapingapi.model;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonInclude.Include;
-import java.time.LocalDate;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDate;
+
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
+@Entity
 public class News {
 
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
   private String title;
   private String url;
+
+  @Column(name = "img_source")
   private String imgSrc;
   private String source;
 
   @JsonInclude(Include.NON_NULL)
+  @Transient
+  @Column(name = "published_date")
   private LocalDate publishedDate;
 
+  @Column(name = "scraped_date")
   private LocalDate scrapedDate;
 }
