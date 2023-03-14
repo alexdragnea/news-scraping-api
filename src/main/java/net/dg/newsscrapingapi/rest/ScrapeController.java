b9b1a34620ec691cfdc3d1b6d340a0b0b2a92961
Coman Laurentiu <laurentiu.coman225@gmail.com>
2023-03-14 12:54:22 +0200

    added logger for scrap controller class

--- src/main/java/net/dg/newsscrapingapi/rest/ScrapeController.java
+++ src/main/java/net/dg/newsscrapingapi/rest/ScrapeController.java
@@ -1,20 +1,27 @@
 package net.dg.newsscrapingapi.rest;
 
-import java.util.List;
 import lombok.AllArgsConstructor;
 import net.dg.newsscrapingapi.model.News;
 import net.dg.newsscrapingapi.service.ScraperService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @AllArgsConstructor
 @RestController
-public class TestController {
+public class ScrapeController {
 
   private final ScraperService scraperService;
+  private static final Logger LOGGER = LoggerFactory.getLogger(ScrapeController.class);
 
   @GetMapping("/latestNews")
   public List<News> latestNews() {
+
+    LOGGER.info("Inside of latestNews method of ScrapeController");
     return scraperService.scrapeNews();
+
   }
 }
