6fd65f13ac1dce0922545fc0148aea8f13014322
Coman Laurentiu <laurentiu.coman225@gmail.com>
2023-03-14 12:56:43 +0200

    refomat style

--- src/main/java/net/dg/newsscrapingapi/rest/ScrapeController.java
+++ src/main/java/net/dg/newsscrapingapi/rest/ScrapeController.java
@@ -1,5 +1,6 @@
 package net.dg.newsscrapingapi.rest;
 
+import java.util.List;
 import lombok.AllArgsConstructor;
 import net.dg.newsscrapingapi.model.News;
 import net.dg.newsscrapingapi.service.ScraperService;
@@ -7,8 +8,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 @AllArgsConstructor
 @RestController
@@ -22,6 +21,5 @@ public class ScrapeController {
 
     LOGGER.info("Inside of latestNews method of ScrapeController");
     return scraperService.scrapeNews();
-
   }
 }
