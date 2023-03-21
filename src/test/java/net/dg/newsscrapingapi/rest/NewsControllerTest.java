ab2cbbc764e82ac3ea82c8c6cd3539b2093c4eda
Antofi Ionut <ionut.antofi@yahoo.com>
2023-03-21 13:44:41 +0200

    add test get all news

--- src/test/java/net/dg/newsscrapingapi/rest/NewsControllerTest.java
+++ src/test/java/net/dg/newsscrapingapi/rest/NewsControllerTest.java
@@ -6,10 +6,14 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
-import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyInt;
 import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(NewsController.class)
 class NewsControllerTest {
@@ -21,7 +25,15 @@ class NewsControllerTest {
   @Test
   void testGetAllNews() throws Exception {
 
-    when(newsService.getNews(any(), any()))
+    when(newsService.getNews(anyInt(), anyInt()))
         .thenReturn(ResponseBodyObjectMother.buildResponseBody());
+
+    mockMvc
+        .perform(
+            MockMvcRequestBuilders.get("/api/v1/news?page=0")
+                .contentType(MediaType.APPLICATION_JSON))
+        .andExpect(status().isOk())
+        .andExpect(jsonPath("$.news").isNotEmpty())
+        .andExpect(jsonPath("$.totalResults").value(5));
   }
 }
