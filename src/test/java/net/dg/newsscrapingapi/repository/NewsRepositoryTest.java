71ded464dc1506dddf6af1cef929399127b53622
alexdragnea <alexdg722@gmail.com>
2023-03-23 14:54:15 +0200

    fix

--- src/test/java/net/dg/newsscrapingapi/repository/NewsRepositoryTest.java
+++ src/test/java/net/dg/newsscrapingapi/repository/NewsRepositoryTest.java
@@ -23,19 +23,19 @@ import org.testcontainers.junit.jupiter.Testcontainers;
 class NewsRepositoryTest {
 
   @Container
-  private static final PostgreSQLContainer<?> database = new PostgreSQLContainer<>("postgres:12.9-alpine");
+  private static final PostgreSQLContainer<?> database =
+      new PostgreSQLContainer<>("postgres:12.9-alpine");
 
   public static class DataSourceInitializer
-          implements ApplicationContextInitializer<ConfigurableApplicationContext> {
+      implements ApplicationContextInitializer<ConfigurableApplicationContext> {
 
     @Override
     public void initialize(ConfigurableApplicationContext applicationContext) {
       TestPropertySourceUtils.addInlinedPropertiesToEnvironment(
-              applicationContext,
-              "spring.datasource.url=" + database.getJdbcUrl(),
-              "spring.datasource.username=" + database.getUsername(),
-              "spring.datasource.password=" + database.getPassword()
-      );
+          applicationContext,
+          "spring.datasource.url=" + database.getJdbcUrl(),
+          "spring.datasource.username=" + database.getUsername(),
+          "spring.datasource.password=" + database.getPassword());
     }
   }
 
